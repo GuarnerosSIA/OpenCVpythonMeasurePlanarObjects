25bf1899ad48b9198f130a1ce443e96700e50ddb
GuarnerosSIA <alejandrowarnsan@gmail.com>
2024-01-10 13:27:38 -0600

    Se agregaron archivos para hacer la calibración sin jupyter

---
+++ image_acquisition.py
@@ -0,0 +1,39 @@
+import cv2
+
+cam = cv2.VideoCapture(0)
+#Camera parameters
+cam.set(11,0)
+cam.set(3,640) #Width
+cam.set(4,480) #Height
+
+cv2.namedWindow("Preview")
+#Create a window to preview the images
+
+img_calibration = 0
+#Creates the calibration numerator to take images
+
+while True:
+    ret, frame = cam.read()#take a picture
+    #Verify if the image was taken
+    if not ret:
+        print("failed to grab frame")
+        break #Exit the application
+    
+    cv2.imshow("Preview", frame)
+    #Show the image in the created window
+    k = cv2.waitKey(1)
+    #Wait for a key commans every milisecond
+    #If a key is pressed, the following condition are asked
+    if k%256 == 27:
+        print("Calibration Image Adquisition Finished")
+        break
+    elif k%256 == 32:
+        img_name = "chessboard images\\Calibration{}.png".format(img_calibration)
+        cv2.imwrite(img_name, frame)
+        print("{} saved!".format(img_name))
+        img_calibration += 1
+        
+
+cam.release()
+
+cv2.destroyAllWindows()
